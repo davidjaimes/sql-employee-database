14190e055ea4ce35a8be0565aaa2fa72f06e537d
David Jaimes <david@djaimes.com>
2020-01-20 14:00:58 -0800

    complete first question

---
+++ queries-djaimes.sql
@@ -0,0 +1,7 @@
+-- 1. List the following details of each employee: employee number, last name, first name, gender, and salary.
+select employees.emp_no, last_name, first_name, gender, salary
+from employees
+inner join salaries
+on employees.emp_no = salaries.emp_no
+
+-- 2. List employees who were hired in 1986.
