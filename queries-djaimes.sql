fbb88287497c4d90307cd56b4149bd21d8b37fd9
David Jaimes <david@djaimes.com>
2020-01-28 22:23:51 -0800

    finish eight question

--- queries-djaimes.sql
+++ queries-djaimes.sql
@@ -83,4 +83,8 @@ on d.dept_no = departments.dept_no
 where dept_name = 'Sales'
 or dept_name = 'Development';
 
--- 8. 
+-- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select last_name, count(last_name) as "Frequency Count"
+from employees
+group by last_name
+order by "Frequency Count" desc;
