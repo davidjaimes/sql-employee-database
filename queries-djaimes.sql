c38d024c03b5bc9ad9038f47b75ff46e8ddc143f
David Jaimes <david@djaimes.com>
2020-01-28 21:25:00 -0800

    finish question 5 from homework

--- queries-djaimes.sql
+++ queries-djaimes.sql
@@ -32,3 +32,9 @@ select emp_no, last_name, first_name, dept_name
 from employee_list
 inner join departments
 on employee_list.dept_no = departments.dept_no;
+
+-- 5. List all employees whose first name is "Hercules" and last names begin with "B."
+select *
+from employees
+where first_name = 'Hercules'
+and last_name like 'B%';
