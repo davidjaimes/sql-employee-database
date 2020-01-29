01ae9086ca5e3ff19042d1f1c73bc640cf9f22c3
David Jaimes <david@djaimes.com>
2020-01-28 20:54:17 -0800

    finish number 4 question

--- queries-djaimes.sql
+++ queries-djaimes.sql
@@ -15,10 +15,20 @@ select dept_manager.dept_no, dept_name, dept_manager.emp_no, from_date, to_date
 from dept_manager
 inner join departments
 on dept_manager.dept_no = departments.dept_no;
-
 select manager_list.dept_no, dept_name, manager_list.emp_no, last_name, first_name, from_date, to_date
 from manager_list
 inner join employees
 on manager_list.emp_no = employees.emp_no
 -- Uncomment next line to get current manager in each department.
 -- where to_date like '9999%';
+
+-- 4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+create view employee_list as
+select employees.emp_no, last_name, first_name, dept_no
+from employees
+inner join dept_emp
+on employees.emp_no = dept_emp.emp_no;
+select emp_no, last_name, first_name, dept_name
+from employee_list
+inner join departments
+on employee_list.dept_no = departments.dept_no;
