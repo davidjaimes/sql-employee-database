6f21640cdfbfd724ebbb36b872de693db0c750d9
David Jaimes <david@djaimes.com>
2020-01-29 23:00:51 -0800

    finish bar chart of average salaries by title

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ connection = engine.connect()
 salaries = pd.read_sql("SELECT * FROM salaries", connection)
 titles = pd.read_sql("SELECT * FROM titles", connection)
 
-'''
+
 plt.style.use('ggplot')
 plt.hist(salaries.salary, bins=20)
 plt.title('Common Salary Ranges for Employees')
@@ -16,8 +16,17 @@ plt.xlabel('Salaries')
 plt.ylabel('Number')
 plt.tight_layout()
 plt.savefig('images/histogram.png', dpi=200)
-'''
 
+
+plt.style.use('seaborn')
 merged = pd.merge(titles, salaries, on='emp_no', how='left')
-means = merged.groupby(title).mean()
-print(means)
+means = merged.groupby('title').mean()
+means.plot(kind='barh', y=['salary'])
+plt.legend().set_visible(False)
+plt.xlabel('Average Salary')
+plt.ylabel('Title')
+plt.tight_layout()
+plt.savefig('images/bar-chart.png', dpi=200)
+
+print(salaries[salaries.emp_no == 499942])
+print(titles[titles.emp_no == 499942])
