9e29c41bea853ee403bbae423e503d8cb87bad84
David Jaimes <david@djaimes.com>
2020-01-30 01:00:22 -0800

    update file

--- schema-djaimes.sql
+++ schema-djaimes.sql
@@ -10,35 +10,35 @@ CREATE TABLE employees (
     first_name varchar(30) NOT NULL,
     last_name varchar(30) NOT NULL,
     gender varchar(1) NOT NULL,
-    hire_date varchar(10) NOT NULL,
+    hire_date varchar(10) NOT NULL
 );
 CREATE TABLE departments (
     dept_no varchar(4) NOT NULL,
-    dept_name varchar(30) NOT NULL,
+    dept_name varchar(30) NOT NULL
 );
 CREATE TABLE dept_emp (
     emp_no int NOT NULL,
     dept_no varchar(4) NOT NULL,
     from_date varchar(10) NOT NULL,
-    to_date varchar(10) NOT NULL,
+    to_date varchar(10) NOT NULL
 );
 CREATE TABLE dept_manager (
     dept_no varchar(4) NOT NULL,
     emp_no int NOT NULL,
     from_date varchar(10) NOT NULL,
-    to_date varchar(10) NOT NULL,
+    to_date varchar(10) NOT NULL
 );
 CREATE TABLE salaries (
     emp_no int NOT NULL,
     salary int NOT NULL,
     from_date varchar(10) NOT NULL,
-    to_date varchar(10) NOT NULL,
+    to_date varchar(10) NOT NULL
 );
 CREATE TABLE titles (
     emp_no int NOT NULL,
     title varchar(30) NOT NULL,
     from_date varchar(10) NOT NULL,
-    to_date varchar(10) NOT NULL,
+    to_date varchar(10) NOT NULL
 );
 
 -- ONLY NEED THIS WITH POSTGRES VERSION GREATER THAN 4.14 OF PGADMIN 4 AND IMPORT/EXPORT INTERFACES DO NOT WORK. THIS IS A CLEVER WORK AROUND.
