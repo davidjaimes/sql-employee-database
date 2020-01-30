d8ff5dc93a35ac93c0c947577c0d3321bf03e29b
David Jaimes <david@djaimes.com>
2020-01-29 18:29:14 -0800

    update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from config import mysql
 from sqlalchemy import create_engine
 engine = create_engine(mysql['host'] + mysql['user'] + mysql['password'] + mysql['database'])
-connection = engine.connect()
+#connection = engine.connect()
