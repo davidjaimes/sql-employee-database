20a70143bde7421bd5eb8e89bcefb7c315cdb5fb
David Jaimes <david@djaimes.com>
2020-01-29 20:55:51 -0800

    update all

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from config import mysql
 from sqlalchemy import create_engine
+import pandas as pd
+
 engine = create_engine(mysql['host'] + mysql['user'] + mysql['password'] + mysql['database'])
-#connection = engine.connect()
+connection = engine.connect()
+data = pd.read_sql("SELECT * FROM employees", connection)
+print(data)
